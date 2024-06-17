241d7e0593e83eb780967bbd24d04b1882dbd391
Quangkubo209 <ngyenngocquang08092002@gmail.com>
2024-06-17 14:14:04 +0700

    fix create user validation

--- src/pages/users/CreateUser.jsx
+++ src/pages/users/CreateUser.jsx
@@ -26,7 +26,6 @@ import { DatePicker } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { useAuthContext } from "../../context/AuthContext";
-import { removeExtraWhitespace } from "../../utils/TrimValue";
 import { CreateUserAPI } from "../../services/users.service";
 
 const PopupNotification = ({

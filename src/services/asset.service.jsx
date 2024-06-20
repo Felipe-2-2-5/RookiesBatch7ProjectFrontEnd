7e2c7b5e4b6acf439fb551668f14070c661487a0
Dang Phan Thanh Cong <dangphanthanhcong.work@gmail.com>
2024-06-20 13:17:56 +0700

    add view asset list feature

--- src/services/asset.service.jsx
+++ src/services/asset.service.jsx
@@ -1,10 +1,26 @@
 import { httpClient } from "../httpClient/httpClient";
 
+export const GetAssets = async () => {
+  const response = await httpClient.get("/assets");
+  return response;
+};
+
 export const GetAsset = async (id) => {
   const response = await httpClient.get(`/assets/${id}`);
   return response;
 };
+
+export const FilterRequest = async (body) => {
+  const response = await httpClient.post("/assets/filter", body);
+  return response;
+};
+
 export const CreateAssetAPI = async (body) => {
   const response = await httpClient.post("/assets", body);
+  return response;
+};
+
+export const GetCategories = async () => {
+  const response = await httpClient.get("/categories");
   return response;
 };
